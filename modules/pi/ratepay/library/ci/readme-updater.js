5ebf7c8988c8e26088d16ec2241933276e9c34f2
Vincent Boulanger <vincent.boulanger@fatchip.de>
2020-12-03 11:40:05 +0100

    initial commit

--- modules/pi/ratepay/library/ci/readme-updater.js
+++
@@ -1,9 +0,0 @@
-const regex = /\| Version    \| `([\d\.]+)`/
-
-module.exports.readVersion = function (contents) {
-  return contents.match(regex)[1]
-}
-
-module.exports.writeVersion = function (contents, version) {
-  return contents.replace(regex, '| Version    | `' + version + '`')
-}
