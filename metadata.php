8460476927cd7f49f685c1ab9245dee5f609e675
markusmichalski-fc <mm$git1972>
2021-02-05 11:24:02 +0100

    added correct version to metadata.php

--- metadata.php
+++ metadata.php
@@ -38,7 +38,7 @@ $aModule = array(
     ),
     'thumbnail'    => 'ratepay_logo.png',
     'lang'         => 'en',
-    'version'      => '5.1.0',
+    'version'      => '6.0.0',
     'author'       => 'Ratepay GmbH',
     'email'        => 'integration@ratepay.com',
     'url'          => 'http://www.ratepay.com/',
