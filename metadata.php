6bc252501fdd41b4e17393b8216f35d37db961ac
Vincent Boulanger <vincent@mail.domain.tld>
2024-07-04 11:30:19 +0200

    Version bump for release

--- metadata.php
+++ metadata.php
@@ -29,7 +29,7 @@ $aModule = array(
     ),
     'thumbnail'    => 'ratepay_logo.png',
     'lang'         => 'en',
-    'version'      => '6.2.0',
+    'version'      => '6.2.1',
     'author'       => 'Ratepay GmbH',
     'email'        => 'integration@ratepay.com',
     'url'          => 'http://www.ratepay.com/',
